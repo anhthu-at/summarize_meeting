ea8030e7859cc733a994802efb9263e6fbb83972
anhthu-at <153301136+anhthu-at@users.noreply.github.com>
2024-10-18 22:06:48 +0700

    Delete backend directory

--- backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-# Register your models here.
-admin.site.register(department)
-admin.site.register(users)
-admin.site.register(meeting)
-admin.site.register(summarize)
